627d1ba7db1737ed1ecc8cb93df5053407f2f78e
Paulo Cesar <pcmorais7@gmail.com>
2021-03-26 18:23:53 -0300

    remove unused

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { Header } from "../../components/Header";
 import api from "../../services/api";
-import Food from "../../components/Food";
+import { Food } from "../../components/Food";
 import { ModalAddFood } from "../../components/ModalAddFood";
 import { ModalEditFood } from "../../components/ModalEditFood";
 import { FoodsContainer } from "./styles";
@@ -13,7 +13,7 @@ export interface FoodProps {
   description: string;
   price: number;
   available: boolean;
-  image: HTMLImageElement;
+  image: string;
 }
 
 export function Dashboard() {
